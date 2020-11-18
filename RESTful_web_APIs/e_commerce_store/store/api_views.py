6e29939d12ff4b4e009a44f8bdfd9cf8d98f7ef4
annalisamf <annalisafoglia@gmail.com>
2020-11-18 16:07:22 +0000

    pagination

--- RESTful_web_APIs/e_commerce_store/store/api_views.py
+++ RESTful_web_APIs/e_commerce_store/store/api_views.py
@@ -1,9 +1,15 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.generics import ListAPIView
+from rest_framework.pagination import LimitOffsetPagination
 
 from store.models import Product
 from store.serializers import ProductSerializer
+
+
+class ProductsPagination(LimitOffsetPagination):
+    default_limit = 10
+    max_limit = 100
 
 
 class ProductList(ListAPIView):
@@ -13,6 +19,9 @@ class ProductList(ListAPIView):
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id',)  # filter by id
     search_fields = ('name', 'description')
+    # show fewer or more products
+    pagination_class = ProductsPagination
+
 
     def get_queryset(self):
         on_sale = self.request.query_params.get('on_sale', None)
