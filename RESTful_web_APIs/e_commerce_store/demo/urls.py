c51be75eb88e9a30735c44dd47ca3326fbe1d483
annalisamf <annalisafoglia@gmail.com>
2020-11-18 15:29:17 +0000

    APIView

--- RESTful_web_APIs/e_commerce_store/demo/urls.py
+++ RESTful_web_APIs/e_commerce_store/demo/urls.py
@@ -4,10 +4,14 @@ from django.contrib import admin
 from django.urls import path
 
 import store.views
+import store.api_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('products/<int:id>/', store.views.show, name='show-product'),
-    path('cart/', store.views.cart, name='shopping-cart'),
-    path('', store.views.index, name='list-products'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  # connect the ListApiView to a route
+                  # allows to sent get request and get back a JSON response
+                  path('api/v1/products', store.api_views.ProductList.as_view()),
+                  path('admin/', admin.site.urls),
+                  path('products/<int:id>/', store.views.show, name='show-product'),
+                  path('cart/', store.views.cart, name='shopping-cart'),
+                  path('', store.views.index, name='list-products'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
