2a04a84640774a55d6235e8682f22a7263f844b6
annalisamf <annalisafoglia@gmail.com>
2020-11-25 19:03:08 +0000

    edit order

--- django_forms/nandias_restaurant/pizza/views.py
+++ django_forms/nandias_restaurant/pizza/views.py
@@ -2,6 +2,7 @@ from django.forms import formset_factory
 from django.shortcuts import render
 
 from .forms import PizzaForm, MultiplePizzaForm
+from .models import Pizza
 
 
 def home(request):
@@ -13,14 +14,16 @@ def order(request):
     if request.method == 'POST':
         filled_form = PizzaForm(request.POST)  # take info from the post and create a new form object
         if filled_form.is_valid():
-            filled_form.save()
+            created_pizza = filled_form.save()
+            created_pizza_pk = created_pizza.id
             size = filled_form.cleaned_data['size']
             topping1 = filled_form.cleaned_data['topping1']
             topping2 = filled_form.cleaned_data['topping2']
             note = f'Thanks for ordering! Your {size} {topping1} and {topping2} pizza is on its way!'
             new_form = PizzaForm()
             return render(request, 'pizza/order.html',
-                          {'pizzaform': new_form, 'note': note, 'multiple_form': multiple_form})
+                          {'created_pizza_pk': created_pizza_pk, 'pizzaform': new_form, 'note': note,
+                           'multiple_form': multiple_form})
     else:
         form = PizzaForm()
         return render(request, 'pizza/order.html', {'pizzaform': form, 'multiple_form': multiple_form})
@@ -44,3 +47,17 @@ def pizzas(request):
         return render(request, 'pizza/pizzas.html', {'note': note, 'formset': formset})
     else:
         return render(request, 'pizza/pizzas.html', {'formset': formset})
+
+
+def edit_order(request, pk):
+    pizza = Pizza.objects.get(pk=pk)  # get the pizza from the db
+    form = PizzaForm(instance=pizza)
+    if request.method == 'POST':
+        filled_form = PizzaForm(request.POST, instance=pizza)
+
+        if filled_form.is_valid():
+            filled_form.save()  # update with the new info
+            form = filled_form
+            note = 'Order has been updated'
+            return render(request, 'pizza/edit_order.html', {'note': note, 'pizzaform': form, 'pizza': pizza})
+    return render(request, 'pizza/edit_order.html', {'pizzaform': form, 'pizza': pizza})
